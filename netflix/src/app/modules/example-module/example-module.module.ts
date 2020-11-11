b598f196b3d202934dd0f5be904926533e4f72a8
Paola Monteiro <paoolamonteiro98@gmail.com>
2020-11-11 20:56:27 -0300

    init example module

---
+++ netflix/src/app/modules/example-module/example-module.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { ExampleComponent } from './components/example/example.component';
+import { ExampleModuleRoutingModule } from './example-module-routing.module';
+
+@NgModule({
+  declarations: [ExampleComponent],
+  imports: [CommonModule, ExampleModuleRoutingModule]
+})
+export class ExampleModuleModule {}
