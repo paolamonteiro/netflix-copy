47243895a4e9e4f33a8686be7643d7a175640801
Suane Vallim <suanedarosav@gmail.com>
2020-12-16 19:45:06 -0300

    autenticação iniciada

--- netflix/src/app/app.module.ts
+++ netflix/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthGuard } from './shared/guard/auth.guard';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), AppRoutingModule],
+  providers: [AuthGuard],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
