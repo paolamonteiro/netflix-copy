acad898363349d3fbbb61b2187795d0c90e05f40
Willian Gomes Espeschit <wgespeschit@gmail.com>
2021-01-06 19:20:08 -0300

    feat: sign up screen created

--- netflix/src/app/shared/services/auth.service.ts
+++ netflix/src/app/shared/services/auth.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { CurrentUser, User, RegisterUser } from '../../modules/login/models/user.model';
-import { UsersService } from '../../modules/login/services/users.service';
+import { UsersService } from './users.service';
+import { debounceTime } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -11,36 +12,50 @@ export class AuthService {
   constructor(private userService: UsersService, private router: Router) {}
 
   public signUp(register: RegisterUser): void {
-    // TODO
+    this.userService.addUser(register).subscribe(
+      user => {
+        this.loginAuth(user);
+      },
+      error => {
+        console.error(error);
+        alert('Erro ao cadastrar');
+      },
+    );
   }
 
-  public loginAuth(currentUser: CurrentUser): boolean {
-    let user: User;
-    this.userService.getAllUsers().subscribe(users => {
-      user = users.find(userInfo => userInfo.email === currentUser.email && userInfo.password === currentUser.password);
-    });
-
-    if (user) {
-      localStorage.setItem('netflix-userLogged', JSON.stringify(user));
-      localStorage.setItem('netflix-status', 'loggedIn');
-      localStorage.setItem('netflix-id', user.id);
-
-      return true;
-    } else {
-      alert('Incorrect e-mail or password');
-      throw new Error('E-mail ou senha incorretos');
-    }
+  public loginAuth(currentUser: CurrentUser): void {
+    this.userService.getAllUsers().subscribe(
+      users => {
+        console.log('users: ', users);
+        const user = users.find(
+          userInfo => userInfo.email === currentUser.email && userInfo.password === currentUser.password,
+        );
+        
+        if (!!user) {
+          localStorage.setItem('netflix-user-logged', JSON.stringify(user));
+          localStorage.setItem('netflix-user-status', 'loggedIn');
+          localStorage.setItem('netflix-user-id', user.id);
+          this.router.navigate(['/home'])
+        } else {
+          throw new Error('E-mail ou senha incorretos');
+        }
+      },
+      error => {
+        console.error(error);
+        alert('E-mail ou senha incorretos');
+      },
+    );
   }
 
   public logOut(): boolean {
-    localStorage.removeItem('netflix-userLogged');
-    localStorage.removeItem('netflix-status');
-    localStorage.removeItem('netflix-id');
+    localStorage.removeItem('netflix-user-logged');
+    localStorage.removeItem('netflix-user-status');
+    localStorage.removeItem('netflix-user-id');
 
     return true;
   }
 
   public getUserId(): string {
-    return localStorage.getItem('netflix-id');
+    return localStorage.getItem('netflix-user-id');
   }
 }
